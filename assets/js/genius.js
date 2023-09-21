989a1cb35828190b43661037b67a7bdbc7b25926
DariaPressley <daria.mik@gmail.com>
2023-09-21 11:52:07 -0700

    adjusting javascript with variables

--- assets/js/genius.js
+++ assets/js/genius.js
@@ -8,16 +8,20 @@
 // } 
 //   searchButton.addEventListener('search', getApi);
 
+
+var artist = document.getElementById ('#modal-artist-input').value;
+var title = document.getElementById ('#modal-song-input').value;
+
 var id;
 
 const settings = {
 	async: true,
 	crossDomain: true,
-	url: 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=beatles+let+it+be&per_page=3&page=1',
+	url: 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=' + artist +title + '&per_page=3&page=1',
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '307037ba87msh679b27d5898dc92p15467bjsnd808822351aa',
-		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com',
 	}
 };
 
@@ -32,11 +36,15 @@ $.ajax(settings).then(function (response) {
     url: 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id='+id,
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '307037ba87msh679b27d5898dc92p15467bjsnd808822351aa',
+      'X-RapidAPI-Key': '6619f90da8msh59c12f6daf73a82p1a280cjsneb6601c06c64',
       'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
     }
   };
   
   $.ajax(settings).done(function (response) {
     console.log(response.lyrics.lyrics.body.html);
-  });
+  }); 
+
+});
+
+

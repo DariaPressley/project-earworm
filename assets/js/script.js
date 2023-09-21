f3153cdd936dcbcde50cb59e518807380610bff1
Nathan Hoang <sliqq123@gmail.com>
2023-09-21 12:00:06 -0700

    added translation functionality for google translate API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
+const geniusLyrics = "English is hard but detectably so" // placeholder for genius lyrics we get from genius API
+
 // myModal.addEventListener('shown.bs.modal', () => {
 //   myInput.focus()
 // })
@@ -15,9 +17,9 @@ const detectoptions = {
 		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
 	},
 	body: new URLSearchParams({
-		q: 'English is hard, but detectably so'
+		q: geniusLyrics // lyrics from genius API//
 	})
-}; 
+};
 
 fetch(detecturl, detectoptions)
     .then(response => {
@@ -27,13 +29,12 @@ fetch(detecturl, detectoptions)
         return response.json();
     })
     .then(data => {
-        const language = data.data.detections[0][0].language;
+        const language = data.data.detections[0][0].language;// detects the language of the original lyrics//
         console.log(language);
 
-        const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=' + language;
-        console.log(url);
+        const getLanguageUrl = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=' + language;
 
-        const options = {
+        const getLanguageOptions = {
             method: 'GET',
             headers: {
                 'Accept-Encoding': 'application/gzip',
@@ -42,15 +43,50 @@ fetch(detecturl, detectoptions)
             }
         };
 
-        fetch(url, options)
+        fetch(getLanguageUrl, getLanguageOptions)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
-                return response.json(); // or response.json() if the result is a JSON
+                return response.json(); 
             })
             .then(result => {
-                console.log(result);
+				for (let i = 0; i < result.data.languages.length; i++) {
+				const availableTranslations = result.data.languages[i].language;
+				
+                console.log(availableTranslations); // return languages available to translate from
+				}
+
+                const translateUrl = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+                const translateOptions = {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded',
+                        'Accept-Encoding': 'application/gzip',
+                        'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
+                        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+                    },
+                    body: new URLSearchParams({
+                        q: geniusLyrics, // lyrics from genius API
+                        target: "es", // target language we want to change the lyrics to
+                        source: language // set source language
+                    })
+                };
+
+                fetch(translateUrl, translateOptions)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then(result => {
+						const translatedLyrics = result.data.translations[0].translatedText
+                        console.log(translatedLyrics); // translated lyrics
+                    })
+                    .catch(error => {
+                        console.error('Fetch error: ' + error.message);
+                    });
             })
             .catch(error => {
                 console.error('Fetch error: ' + error.message);

36553f62d6cf0599eb46d8a181bcfd4a72bde3c7
Nathan Hoang <sliqq123@gmail.com>
2023-09-21 11:10:39 -0700

    added get languages javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
 
-const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
-const options = {
+const detecturl = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+const detectoptions = {
 	method: 'POST',
 	headers: {
 		'content-type': 'application/x-www-form-urlencoded',
@@ -17,14 +17,45 @@ const options = {
 	body: new URLSearchParams({
 		q: 'English is hard, but detectably so'
 	})
-};
+}; 
 
-fetch(url, options)
-	.then(response => response.json())
-	.then(data => {
-		const language = data.data.detections[0][0].language;
-		console.log(language);
-	})
-	.catch(error => {
-		console.error(error);
-	});
+fetch(detecturl, detectoptions)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const language = data.data.detections[0][0].language;
+        console.log(language);
+
+        const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=' + language;
+        console.log(url);
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'Accept-Encoding': 'application/gzip',
+                'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
+                'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+            }
+        };
+
+        fetch(url, options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json(); // or response.json() if the result is a JSON
+            })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.error('Fetch error: ' + error.message);
+            });
+    })
+    .catch(error => {
+        console.error(error);
+    });

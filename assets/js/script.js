d44d6bfe5af14ac0756cd8953c338f86e739904c
Nathan Hoang <sliqq123@gmail.com>
2023-09-21 10:17:12 -0700

    translate api detection

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'Accept-Encoding': 'application/gzip',
+		'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
+		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+	},
+	body: new URLSearchParams({
+		q: 'English is hard, but detectably so'
+	})
+};
+
+fetch(url, options)
+	.then(response => response.json())
+	.then(data => {
+		const language = data.data.detections[0][0].language;
+		console.log(language);
+	})
+	.catch(error => {
+		console.error(error);
+	});

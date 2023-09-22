df5e8c2ba15ca6e472386f4ea9393442fa6d7c8d
Nathan Hoang <sliqq123@gmail.com>
2023-09-22 14:33:46 -0700

    added local storage for user input

--- assets/js/genius.js
+++ assets/js/genius.js
@@ -1,66 +1,174 @@
-var songEL = document.getElementById('song');
-var artistEl = document.getElementById('artist');
-var lyricsEl = document.getElementById ('lyrics');
-var searchEl = document.getElementById ('search');
+var artistName = localStorage.getItem("artistName");
+var artistText = document.getElementById("artist-card-text")
 
-var artist = document.getElementById('modal-artist-input');
-var title = document.getElementById ('modal-song-input');
-var saveArtist = document.getElementById ("artist-submit");
-var saveSong = document.getElementById ("song-submit");
-var artistArea = document.getElementById ("find-artist-textarea")
-var songArea = document.getElementById("find-song-textarea")
+function getArtistName () {
+    var artistName = localStorage.getItem("artistName");
+    var artistText = document.getElementById("artist-card-text")
+    artistText.textContent = artistName
+}
 
-saveArtist.addEventListener ('click', function (event) {
-  event.preventDefault();
-  artistArea.textContent=artist.value
-  console.log(artist)
-  });
+function getSongName () {
+    var songName = localStorage.getItem("songName");
+    var songText = document.getElementById("song-title-text");
+    songText.textContent = songName
+}
+
+function getSourceLang () {
+    var sourceLang = localStorage.getItem("sourceLangEng");
+    var sourceLangtext = document.getElementById("language-card-text");
+    sourceLangtext.textContent = sourceLang
+
+}
+
+function getTranslateLang () {
+    var translateLang = localStorage.getItem("translateLangEng")
+    var translateLangtext = document.getElementById("language-card-text")
+    translateLangtext.textContent = translateLang
+}
+
+getArtistName();
+getSongName();
+getSourceLang();
 
 
-saveSong.addEventListener ('click', function (event) {
-    event.preventDefault();
-    songArea.textContent=title.value
-    console.log(artist)
-    });
 
 
 
-// var id;
-// function getApi() {
-//     var requestUrl = 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=%3CREQUIRED%3E&per_page=3&page=1';
-// } 
-//   searchButton.addEventListener('search', getApi);
 
-// var id;
 
-// const settings = {
-// 	async: true,
-// 	crossDomain: true,
-// 	url: 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=beatles+let+it+be&per_page=3&page=1',
+// const songName = "All of me"; // Replace with user input
+// const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=' + songName + '&per_page=10&page=1'; // Genius API search call
+// const options = {
 // 	method: 'GET',
 // 	headers: {
-// 		'X-RapidAPI-Key': '307037ba87msh679b27d5898dc92p15467bjsnd808822351aa',
+// 		'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
 // 		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
 // 	}
 // };
 
-// $.ajax(settings).then(function (response) {
-// 	console.log(response.sections[0].hits[0].result.id);
-//   id = response.sections[0].hits[0].result.id
+// fetch(url, options)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+//         return response.json();
+//     })
+//     .then(result => { // Getting all the info we need from whatever the user searched
+//         console.log(result)
+//         const artistName = result.hits[0].result.primary_artist.name; // artist name
+//         const songTitle = result.hits[0].result.full_title; // song title
+//         const songImage = result.hits[0].result.song_art_image_url; // song image
+//         const songId = result.hits[0].result.id; // song id
 
+//         const url = 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=' + songId; // Genius API song lyric call
+//         const options = {
+//             method: 'GET',
+//             headers: {
+//                 'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
+//                 'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+//             }
+//         };
 
-//   const settings = {
-//     async: true,
-//     crossDomain: true,
-//     url: 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id='+id,
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '307037ba87msh679b27d5898dc92p15467bjsnd808822351aa',
-//       'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
-//     }
-//   };
-  
-//   $.ajax(settings).done(function (response) {
-//     console.log(response.lyrics.lyrics.body.html);
-//   });
+//         fetch(url, options)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error(`HTTP error! Status: ${response.status}`);
+//                 }
+//                 return response.json();
+//             })
+//             .then(result => {
+//                 console.log(result)
+//                 const originalLyrics = result.lyrics.lyrics.body.html; // lyrics generated from genius API
+//                 console.log(originalLyrics);
 
+//                 const detecturl = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';  // Detect language call with Translate API
+//                 const detectoptions = {
+//                     method: 'POST',
+//                     headers: {
+//                         'content-type': 'application/x-www-form-urlencoded',
+//                         'Accept-Encoding': 'application/gzip',
+//                         'X-RapidAPI-Key': '6619f90da8msh59c12f6daf73a82p1a280cjsneb6601c06c64',
+//                         'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+//                     },
+//                     body: new URLSearchParams({
+//                         q: originalLyrics // lyrics from genius API
+//                     })
+//                 };
+
+//                 fetch(detecturl, detectoptions)
+//                     .then(response => {
+//                         if (!response.ok) {
+//                             throw new Error(`HTTP error! Status: ${response.status}`);
+//                         }
+//                         return response.json();
+//                     })
+//                     .then(data => {
+//                         console.log(data)
+//                         const language = data.data.detections[0][0].language; // detects the language of the original lyrics
+//                         console.log(language);
+
+//                         const getLanguageUrl = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=' + language;
+//                         const getLanguageOptions = {
+//                             method: 'GET',
+//                             headers: {
+//                                 'Accept-Encoding': 'application/gzip',
+//                                 'X-RapidAPI-Key': '6619f90da8msh59c12f6daf73a82p1a280cjsneb6601c06c64',
+//                                 'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+//                             }
+//                         };
+
+//                         fetch(getLanguageUrl, getLanguageOptions)
+//                             .then(response => {
+//                                 if (!response.ok) {
+//                                     throw new Error(`HTTP error! Status: ${response.status}`);
+//                                 }
+//                                 return response.json();
+//                             })
+//                             .then(result => {
+//                                 console.log(result)
+//                                 for (let i = 0; i < result.data.languages.length; i++) { //for loop that iterates through all data languages
+//                                     console.log(result)
+//                                     const availableTranslations = result.data.languages[i].language; 
+//                                     const availableTranslationsEng = result.data.languages[i].name;
+                                    
+//                                     console.log(availableTranslations); // return language codes available to translate from
+//                                     console.log(availableTranslationsEng); // returns available translations in English
+//                                 }
+
+//                                 const translateUrl = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+//                                 const translateOptions = {
+//                                     method: 'POST',
+//                                     headers: {
+//                                         'content-type': 'application/x-www-form-urlencoded',
+//                                         'Accept-Encoding': 'application/gzip',
+//                                         'X-RapidAPI-Key': '6619f90da8msh59c12f6daf73a82p1a280cjsneb6601c06c64',
+//                                         'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+//                                     },
+//                                     body: new URLSearchParams({
+//                                         q: originalLyrics, // lyrics from genius API
+//                                         target: "es", // target language code we want to change the lyrics to. replace later with user input.
+//                                         source: language // set source language
+//                                     })
+//                                 };
+
+//                                 fetch(translateUrl, translateOptions)
+//                                     .then(response => {
+//                                         if (!response.ok) {
+//                                             throw new Error(`HTTP error! Status: ${response.status}`);
+//                                         }
+//                                         return response.json();
+//                                     })
+//                                     .then(result => {
+//                                         console.log(result)
+//                                         const translatedLyrics = result.data.translations[0].translatedText;
+//                                         console.log(translatedLyrics); // translated lyrics
+//                                     })
+//                                     .catch(error => console.error('Fetch error: ' + error.message));
+//                             })
+//                             .catch(error => console.error('Fetch error: ' + error.message));
+//                     })
+//                     .catch(error => console.error(error));
+//             })
+//             .catch(error => console.error(error));
+//     })
+//     .catch(error => console.error(error));
